a2a57919807b2b4cc1cbd6a37dc202ff93362fc9
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-07-09 09:39:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pro-asp.net-core-identity-Mike-test
+ Mike test asp.net core identity
---
+++ chapter-2-your first Identity Application/IdentityTodo/Data/TodoItem.cs
@@ -0,0 +1,12 @@
+namespace IdentityTodo.Data
+{
+    public class TodoItem
+    {
+        public long Id{get;set;}
+        public string? Task {get;set;}
+        
+        public bool Complete {get;set;}
+        
+        public string? Owner {get;set;}
+    }
+}
