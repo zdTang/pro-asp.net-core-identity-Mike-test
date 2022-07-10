82f7db2faa6fe2f019bde971520f2bdd2a05a62a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2022-07-10 08:18:40 -0400

    3-Config the Application

--- 3-IdentityApp/Program.cs
+++ 3-IdentityApp/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using IdentityApp.Models;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+// Here to setup Services
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ProductDbContext>(opts => {
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:AppDataConnection"]);
+});
+
+builder.Services.AddHttpsRedirection(opts => {
+    opts.HttpsPort = 44350;
+});
+
+var app = builder.Build();
+// Here to setup pipeline
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
