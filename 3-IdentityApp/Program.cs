692249e2fe19f781641d0fda875c02fd7d5e9933
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-10-03 08:32:29 -0400

    4-P63-Configuring the App, add LoginPartial View

--- 3-IdentityApp/Program.cs
+++ 3-IdentityApp/Program.cs
@@ -1,16 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ProductDbContext>(opts => {
+
+builder.Services.AddDbContext<ProductDbContext>(opts =>
+{
     opts.UseSqlServer(
         builder.Configuration["ConnectionStrings:AppDataConnection"]);
 });
 
-builder.Services.AddHttpsRedirection(opts => {
+builder.Services.AddDbContext<IdentityDbContext>(opts =>
+{
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:IdentityConnection"],
+        opts => opts.MigrationsAssembly("IdentityApp")
+    );
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<IdentityDbContext>();
+
+builder.Services.AddHttpsRedirection(opts =>
+{
     opts.HttpsPort = 44350; // override defautl HTTPS PORT 443
 });
 
